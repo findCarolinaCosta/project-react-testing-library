af4313db69d457b498b0c81600f5a53610b931d0
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-05 17:37:59 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Requisito 3. Teste o componente <FavoritePokemons.js />', () => {
+  beforeEach(() => {
+    render(
+      <FavoritePokemons />,
+    );
+  });
+
+  test(`Teste se é exibido na tela a mensagem No 
+  favorite pokemon found, se a pessoa não tiver pokémons favoritos`, () => {
+    const nothingFoundText = screen.getByText('No favorite pokemon found');
+    expect(nothingFoundText).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const btnMoreDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(btnMoreDetails);
+
+    const favoriteCheck = screen.getByRole('checkbox');
+    userEvent.click(favoriteCheck);
+
+    const linkFavs = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavs);
+  });
+});
