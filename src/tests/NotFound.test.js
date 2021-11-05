254c9e2be9d79ba49ff425ce9f635abc28c11c7e
findcarolinacosta <carolinadacosta1997@gmail.com>
2021-11-05 18:08:09 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Requisito 4. Teste o componente <NotFound.js />', () => {
+  beforeEach(() => {
+    render(
+      <NotFound />,
+    );
+  });
+  test(`Teste se página contém um heading h2 com 
+o texto Page requested not found 😭`, () => {
+    const notFoundText = screen.getByRole('heading', { name:
+        /Page requested not found/i });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    const img = screen.getByRole('img', { name: /Pikachu/i });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
